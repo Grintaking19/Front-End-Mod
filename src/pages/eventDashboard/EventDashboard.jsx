fe791b3f429d2ff4c135d8ecf9cf7d991e44217b
khalid <khalidmamdou7@gmail.com>
2023-03-24 00:13:18 +0200

    dashboard: Create the grid layout (navbars,sidebars, ..)
    Add a dummy navbar in addition to sidebars components to the dashboard.
    Add a body container.
    
    Signed-off-by: khalid <khalidmamdou7@gmail.com>

--- src/pages/eventDashboard/EventDashboard.jsx
+++ src/pages/eventDashboard/EventDashboard.jsx
@@ -1,8 +1,18 @@
 import "./eventDashboard.css";
+import CreatorSidenav from "../../layouts/creatorSidenav/CreatorSidenav";
+import EventSidenav from "../../layouts/eventSidenav/EventSidenav";
 
 function EventDashboard() {
     return (
         <div className="event-dashboard">
+            <div className="navbar-k">
+            </div>
+            <CreatorSidenav />
+            <EventSidenav />
+            <div className="body-container">
+                <div className="content-container">
+                </div>
+            </div>
         </div>
     );
 }
