d3b93a25cb3019f2ba6379ccfe6c4f73573838ea
khalid <khalidmamdou7@gmail.com>
2023-04-15 19:20:25 +0200

    Refactor Card css to module css

--- src/pages/event-dashboard/card/Card.jsx
+++ src/pages/event-dashboard/card/Card.jsx
@@ -2,21 +2,21 @@ import './Card.module.css';
 
 function Card(props) {
     return (
-        <div className="card-container">
-            <div className="dashboard-card">
-                <div className="card-header">
+        <div className={styles=['card-container']}>
+            <div className={styles['dashboard-card']}>
+                <div className={styles['card-header']}>
                     {props.title}
                 </div>
-                <div className="card-body">
-                    <div className="card-body-main-content">
+                <div className={styles['card-body']}>
+                    <div className={styles['card-body-main-content']}>
                         <span>{props.mainContent}</span>
-                        <span className="card-body-main-content-sub">{props.subMainContent}</span>
+                        <span className={styles['card-body-main-content-sub']}>{props.subMainContent}</span>
                     </div>
-                    <div className="card-body-sub-content">
+                    <div className={styles['card-body-sub-content']}>
                         <span>{props.subContent}</span>
                     </div>
                 </div>
-                <div className="card-footer">
+                <div className={styles['card-footer']}>
                     <p>{props.footer}</p>
                 </div>
             </div>
