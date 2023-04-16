1ee2f2a1a525c127623f95ff0b1df2bbcb883c09
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2023-04-16 17:39:52 +0200

    Added pagination2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import LandingPage from "./pages/landing-page/LandingPage"
 import EventDashboard from "./pages/event-dashboard/EventDashboard"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EventPage from "./pages/event-page/EventPage"
-import SignIn from "./pages/sign-in/SignIn"
-import SignUpPage from "./pages/sign-up/SignUpPage"
-import ForgotPasswordPage from "./pages/forgot-password/ForgotPasswordPage"
-import ResetPassordPage from "./pages/reset-password/ResetPassword"
+// import SignIn from "./pages/sign-in/SignIn"
+// import SignUpPage from "./pages/sign-up/SignUpPage"
+// import ForgotPasswordPage from "./pages/forgot-password/ForgotPasswordPage"
+// import ResetPassordPage from "./pages/reset-password/ResetPassword"
 
 export default function App() {
 
@@ -22,10 +22,10 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/event-dashboard" element={<EventDashboard />} />
         <Route path="/events/:eventId" element={<EventPage />} />
-        <Route path="/login" element={<SignIn />} />
+        {/* <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPassordPage />} />
+        <Route path="/reset-password" element={<ResetPassordPage />} /> */}
       </Routes>
     </BrowserRouter>
 
