8b0c156096be8054a3a9c3146f55674537f38560
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2023-04-29 17:01:32 +0200

    Dynamic pagination by sending requests to DB on each page

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -2,7 +2,6 @@ import {React,useState} from "react";
 import Events from "./events/Events"
 import NavBar from "../../layouts/navbar/NavBar"
 import Categories from "./categories/Categories"
-import SignedOutNavBar from "../../layouts/navbar/SignedOutNavBar";
 import cover from "../../pages/landing-page/cover.png";
 
 export default function LandingPage() {
@@ -13,7 +12,7 @@ export default function LandingPage() {
     return (
         <div>
             <NavBar/>
-        <img id="cover" src={cover} width="1519.2px" />
+        <img id="cover" src={cover} width="100%" />
      
             <Categories activeTab={activeTab} setActiveTab={setActiveTab} location={location} setLocation={setLocation}/>
             <Events activeTab={activeTab} location={location} />
