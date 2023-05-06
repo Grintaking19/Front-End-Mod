fa6e5c19fd4b770607938b8f4f5f6312f2acbff0
khalid <khalidmamdou7@gmail.com>
2023-05-06 06:47:10 +0300

    sidenav: add name props to use instead of fetched Data

--- src/layouts/event-sidenav/EventSidenav.jsx
+++ src/layouts/event-sidenav/EventSidenav.jsx
@@ -5,68 +5,48 @@ import { Link, Navigate } from "react-router-dom"
 
 import { getTicketTypes } from "../../pages/event-dashboard/services"
 import { getEvent } from "./services"
+import { icons, eventPublishSteps } from "./data"
 
+/**
+ * Component for the sidenav of the event creation pages
+ * 
+ * @component
+ * @param {Object} props
+ * @param {string} props.eventId - The id of the event (Mandatory if the event has been created in the backend)
+ * @param {string} props.activeTab - The active tab of the sidenav
+ * @param {string} props.eventName - The name of the event (Mandatory if the event has not been created in the backend yet and no eventId is provided)
+ * @param {string} props.startDate - The start date of the event (Optional, will be displayed as TBD if not provided)
+ * 
+ * @example
+ * const activeTab = 'details'
+ * const eventName = 'Event Name'
+ * return (
+ *  <EventSidenav activeTab={activeTab} eventName={eventName} />
+ * )
+ * 
+ * @example
+ * const eventId = '123'
+ * const activeTab = 'details'
+ * return (
+ * <EventSidenav eventId={eventId} activeTab={activeTab} />
+ * )
+ */
 function EventSidenav(props) {
-    const { eventId, activeStep } = props
-
-    let eventPublishSteps = [
-        {
-            id: 1,
-            title: 'Basic Info',
-            link: '/event/basic-info',
-            status: 'completed'
-        }, {
-            id: 2,
-            title: 'Details',
-            link: '/event-details',
-            status: 'not-completed'
-        }, {
-            id: 3,
-            title: 'Tickets',
-            link: '/event/tickets',
-            status: 'not-completed'
-        }, {
-            id: 4,
-            title: 'Publish',
-            link: '/publish-event',
-            status: 'not-completed'
-        }, {
-            title: 'Dashboard',
-            link: '/event/dashboard',
-            selected: true
-        }, {
-            title: 'Orders',
-            link: '/event/orders',
-        }, {
-            title: 'Add Attendees',
-            link: '/event/add-attendees',
-        }
-    ]
-
-    let icons = {
-        completed: [<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-        </svg>],
-        1: [<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-1-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312V4.002Z" />
-        </svg>],
-        2: [<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-2-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z" />
-        </svg>],
-        3: [<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-3-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z" />
-        </svg>],
-        4: [<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-4-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM7.519 5.057c-.886 1.418-1.772 2.838-2.542 4.265v1.12H8.85V12h1.26v-1.559h1.007V9.334H10.11V4.002H8.176c-.218.352-.438.703-.657 1.055ZM6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218Z" />
-        </svg>]
-    }
+    // TODO: Remove the eventName and startDate props and use the eventId to fetch the event details if the backend have implemented edit event details
+    const { eventId, activeTab, eventName, startDate } = props
+    
+    // TODO: Use the activeTab prop to set the active tab
 
     const [isDraft, setIsDraft] = useState(true)
-    const [canBePublished, setCanBePublished] = useState(true)
-    const [eventDetails, setEventDetails] = useState({})
+    const [canBePublished, setCanBePublished] = useState(false)
+    const [eventDetails, setEventDetails] = useState({
+        name: eventName,
+        startDate: startDate || 'TBD'
+    })
 
     useEffect(() => {
         async function fetchData() {
+            if (!eventId) return
             const eventDetailsData = await getEvent(eventId)
             setEventDetails(eventDetailsData)
             setIsDraft(eventDetailsData.draft)
@@ -124,6 +104,7 @@ function EventSidenav(props) {
                         {/* <p>Wed, Apr 19, 2023 7:00 PM</p> */}
                         <p id="event-date">{`${eventDetails.startDate}`}</p>
                     </div>
+                    {!isDraft ?
                     <div className={styles['es-event-description-preview']}>
                         {/* <a href="#" id="preview-event"> */}
                         <Link to={`/events/${eventId}`} id="preview-event">
@@ -135,6 +116,7 @@ function EventSidenav(props) {
                         {/* </a> */}
                         </Link>
                     </div>
+                    : null}
                 </div>
             </div>
             <div className={styles['es-list']}>
