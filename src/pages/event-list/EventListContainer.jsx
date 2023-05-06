2bcb92d0e0681fcce77bf7177efe0f0217c5a2ac
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2023-05-06 14:15:56 +0300

    Adding navbar to eventlist

--- src/pages/event-list/EventListContainer.jsx
+++ src/pages/event-list/EventListContainer.jsx
@@ -3,6 +3,7 @@ import PromoteEvent from "./promote-event/PromoteEvent";
 import List from "./event-list/List";
 import OptionsBar from "./options-bar/OptionsBar";
 import { useState } from "react";
+import NavBar from "../../layouts/navbar/NavBar";
 const EventListConatiner = (props) => {
 
   const [view, setView] = useState("future");
@@ -17,12 +18,17 @@ const EventListConatiner = (props) => {
   }
   
   return(
+    <div> 
+          <NavBar/>
+
   <div className= {styles['event-list-container']} id="event-list-container">
     <h1 className={styles['header']} id="event-list-header"> Events</h1>
     <OptionsBar onViewChange={onViewChangeHandler} onUserInput={onUserInputHandler}/>
     <PromoteEvent />
     <List listView = {view} searchInput={userSearchinput}/>
   </div>
+  </div>
+
   )
 };
 
