e7ad51d185ccdc81c61477a9420033412a65c174
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2023-05-07 22:20:00 +0300

    Add CSV export

--- src/pages/event-list/EventListContainer.jsx
+++ src/pages/event-list/EventListContainer.jsx
@@ -4,7 +4,25 @@ import List from "./event-list/List";
 import OptionsBar from "./options-bar/OptionsBar";
 import { useState } from "react";
 import NavBar from "../../layouts/navbar/NavBar";
+import Button from "../../layouts/UI/Button";
+import axios from "axios";
+
+const getCSVRequest = async () => {
+  let url = "https://www.hebtus.me/api/v1/creators/events/?page=1&limit=10&csv=true";
+  let config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user"),
+      "ngrok-skip-browser-warning": "1",
+      mode: "no-cors",
+    },
+  };
+  let response = await axios.get(url,config);
+  return response;
+};
+
 const EventListConatiner = (props) => {
+
+  const CSVButtonStyle = {"width":"15%", "marginBottom":"30px", "marginTop":"20px"}
 
   const [view, setView] = useState("future");
   const [userSearchinput, setUserSearchInput] = useState("");
@@ -16,6 +34,19 @@ const EventListConatiner = (props) => {
   const onUserInputHandler = (event) => {
     setUserSearchInput(event.target.value);
   }
+
+  const downloadCSVHandler = async ()  => {
+    let response = await getCSVRequest();
+
+    const blob = new Blob([response.data], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = "Events.csv";
+    link.href = url;
+    link.click();
+    console.log(response);
+
+  }
   
   return(
     <div> 
@@ -26,6 +57,8 @@ const EventListConatiner = (props) => {
     <OptionsBar onViewChange={onViewChangeHandler} onUserInput={onUserInputHandler}/>
     <PromoteEvent />
     <List listView = {view} searchInput={userSearchinput}/>
+    <Button style={CSVButtonStyle} onClick={downloadCSVHandler}> Download CSV</Button>
+    
   </div>
   </div>
 
