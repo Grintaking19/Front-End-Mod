7230626d32ffffbe5206ec0b928f872814a8ff76
khalid <khalidmamdou7@gmail.com>
2023-05-09 22:59:16 +0300

    refactor: edit dates components width
    JSDocs updated

--- src/pages/create-event/date-and-time/DateTime.jsx
+++ src/pages/create-event/date-and-time/DateTime.jsx
@@ -147,7 +147,7 @@ const DateTime = (props) => {
       <HorizontalFlex width="100%" id="start-date-time-flex">
         <BasicDatePicker
           title="Start Date *"
-          width="49%"
+          width="100%"
           required={1}
           onChange={onStartDateChangeHandler}
           error={dateError}
@@ -167,7 +167,7 @@ const DateTime = (props) => {
       <HorizontalFlex width="100%" id="end-date-time-flex">
         <BasicDatePicker
           title="End Date *"
-          width="49%"
+          width="100%"
           required={1}
           onChange={onEndDateChangeHandler}
           error={dateError}
--- src/pages/create-event/fields/BasicDatePicker.jsx
+++ src/pages/create-event/fields/BasicDatePicker.jsx
@@ -1,43 +1,43 @@
- import   "./BasicDatePicker.css";
+import "./BasicDatePicker.css";
 
- import { Fragment, useState } from "react";
- import {CalendarIcon} from "../../../layouts/UI/SvgImages";
- import { DatePicker } from '@mui/x-date-pickers/DatePicker';
- import dayjs from 'dayjs';
- import { LocalizationProvider } from "@mui/x-date-pickers";
- import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Fragment, useState } from "react";
+import { CalendarIcon } from "../../../layouts/UI/SvgImages";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 
- const BasicDatePicker = (props) => {
+const BasicDatePicker = (props) => {
 
-   const dateError = () => {
-     if (props.error){
-       return ({border: "1px solid #c5162e"})
-     }
-     else {
-       return ({})
-     }
-   }
-   const onDateChange = (date) => {
-     props.onChange(date);
-   }
+  const dateError = () => {
+    if (props.error) {
+      return ({ border: "1px solid #c5162e" })
+    }
+    else {
+      return ({})
+    }
+  }
+  const onDateChange = (date) => {
+    props.onChange(date);
+  }
 
-   let value = new Date()+1;
-   return (
-     <LocalizationProvider dateAdapter={AdapterDayjs}>
+  let value = new Date() + 1;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
 
-     <div id={props.id}>
+      <div id={props.id} style={{ width: props.width }} className={"date-picker-container"}>
 
-     <DatePicker sx={{width:"310px", border: dateError()}} onChange={onDateChange} 
-     defaultValue={dayjs(new Date(props.value))}
-     disabled={props.disabled}
-     />
-     <p className={"date-picker-title"}> {props.title}</p> 
-     {(props.error) && <p className={"date-picker-warning"}>End date must be after start date. </p>}
-     </div>
-     </LocalizationProvider>
+        <DatePicker sx={{ border: dateError() }} onChange={onDateChange}
+          defaultValue={dayjs(new Date(props.value))}
+          disabled={props.disabled}
+        />
+        <p className={"date-picker-title"}> {props.title}</p>
+        {(props.error) && <p className={"date-picker-warning"}>End date must be after start date. </p>}
+      </div>
+    </LocalizationProvider>
 
-   )
- }
+  )
+}
 
- export default BasicDatePicker;
+export default BasicDatePicker;
