0c9b92eca4a9d02066be59bb307a519a336a75a4
khalid <khalidmamdou7@gmail.com>
2023-05-09 22:55:09 +0300

    refactor: made ui-panel component image, title, desc optional
    JSDocs updated

--- src/layouts/UI/Panel.jsx
+++ src/layouts/UI/Panel.jsx
@@ -3,14 +3,16 @@ import styles from './Panel.module.css';
 const Panel = (props) => {
     return (
         <div className={styles["panel"]} style={props.style}>
-        <div className={styles["panel-image"]}>
-            {props.image()}
-        </div>
-        <div className={styles["panel-body"]}>
-            <h1 className={styles["panel-title"]}>{props.title}</h1>
-            <p className={styles["panel-description"]}>{props.description}</p>
-            {props.children}
-        </div>
+            {props.image ?
+                <div className={styles["panel-image"]}>
+                    {props.image()}
+                </div>
+                : null}
+            <div className={styles["panel-body"]}>
+                {props.title ? <h1 className={styles["panel-title"]}>{props.title}</h1> : null}
+                {props.description ? <p className={styles["panel-description"]}>{props.description}</p> : null}
+                {props.children}
+            </div>
         </div>
     );
 
