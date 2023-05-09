5e8302d2e62e978c5e5406c4e4bc7f16dd927b29
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2023-05-09 22:22:13 +0200

    JsDocs LandingPage 2
    JSDocs updated

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -5,8 +5,13 @@ import Categories from "./categories/Categories"
 import cover from "../../pages/landing-page/cover.png";
 
 /**
-    Renders the landing page component that encapsulates the navbar, a cover image, categories, and events cards.
-    The landing page component is the entry point of the website 
+    Renders the landing page component which is the entry point of the website 
+    The component returns a JSX code that renders a navigation bar, an image, a Categories component, and an Events component.
+    The useState hook is used to create two state variables: activeTab and location which are initially set to empty strings. 
+    These variables are passed as props to the Categories and Events components along with two functions setActiveTab and setLocation that can be used to update their values.
+    The NavBar component is rendered first, followed by an img tag that displays the cover image using the src attribute.
+    The Categories and Events components are rendered with their respective props. The activeTab prop is used to highlight the active category tab in the Categories component, while the location prop is used to filter events by location in the Events component.
+    Overall, this code defines a landing page component that displays a navigation bar, an image, and two components for displaying and filtering events by category and location.
     @component LandingPage
     @name LandingPage
     @returns {JSX.Element} The Landing Page
