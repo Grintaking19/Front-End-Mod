f3f0b74bc4ddf5643af7b0e5e7bf09a70c9ba391
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2023-05-09 22:19:21 +0200

    JsDocs LandingPage
    JSDocs updated

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -4,11 +4,21 @@ import NavBar from "../../layouts/navbar/NavBar"
 import Categories from "./categories/Categories"
 import cover from "../../pages/landing-page/cover.png";
 
+/**
+    Renders the landing page component that encapsulates the navbar, a cover image, categories, and events cards.
+    The landing page component is the entry point of the website 
+    @component LandingPage
+    @name LandingPage
+    @returns {JSX.Element} The Landing Page
+    @example
+    return (<LandingPage/>)
+    */
+
+
 export default function LandingPage() {
     const [activeTab, setActiveTab] = useState('');
     const [location, setLocation] = useState('');
    
-
     return (
         <div>
             <NavBar/>
