5e1ed8d1f420291b739295f3bb5d6c2e7fcab45c
Omar Badr <omarbadrh.ob@gmail.com>
2023-05-11 04:12:30 +0200

    UI fixes while waiting merging
    JSDocs updated

--- src/pages/booking/CancelSessionPage.jsx
+++ src/pages/booking/CancelSessionPage.jsx
@@ -1,19 +1,19 @@
 import styles from "./CancelSessionPage.module.css"
 
-export function CancelSessionPage({ setModal, setShowCloseMessage,setCheckout }) {
+export function CancelSessionPage({ setModal, setShowCloseMessage, setCheckout }) {
 
   return (
-    
-      <div className={styles["page"]}>
-        <p className={styles["page--title"]}>Leave Checkout?</p>
-        <div className={styles["page--content-container"]}>
-          <p className={styles["page--content"]}>Are you sure you want to leave checkout? The items you've selected may not be available later.</p>
-          <div className={styles["page--buttons-containers"]}>
-            <button className={styles["button-stay"]} onClick={() => setShowCloseMessage(false)}>Stay</button>
+
+    <div className={styles["page"]}>
+      <p className={styles["page--title"]}>Leave Checkout?</p>
+      <div className={styles["page--content-container"]}>
+        <p className={styles["page--content"]}>Are you sure you want to leave checkout? The items you've selected may not be available later.</p>
+        <div className={styles["page--buttons-containers"]}>
+          <button className={styles["button-stay"]} onClick={() => setShowCloseMessage(false)}>Stay</button>
           <button className={styles["button-leave"]} onClick={() => { setModal(false); setCheckout(false); }}>Leave</button>
-          </div>
         </div>
       </div>
-    
+    </div>
+
   )
 }
