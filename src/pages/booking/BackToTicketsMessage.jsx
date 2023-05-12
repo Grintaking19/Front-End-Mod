80ed660c8a002e7695474e55e273d8ba20d43320
Omar Badr <omarbadrh.ob@gmail.com>
2023-05-12 02:27:09 +0200

    IDs is added to booking
    JSDocs updated

--- src/pages/booking/BackToTicketsMessage.jsx
+++ src/pages/booking/BackToTicketsMessage.jsx
@@ -3,13 +3,13 @@ import styles from "./CancelSessionPage.module.css"
 export function BackToTicketsMessage({ setCheckout, setShowBackToTicketsMessage }) {
 
   return (
-    <div className={styles["page"]}>
-      <p className={styles["page--title"]}>Changed your mind?</p>
-      <div className={styles["page--content-container"]}>
-        <p className={styles["page--content"]}>Are you sure you want to leave checkout, and go back to ? The items you've selected may not be available later.</p>
-        <div className={styles["page--buttons-containers"]}>
-          <button className={styles["button-stay"]} onClick={()=>setShowBackToTicketsMessage(false)}>Stay</button>
-          <button className={styles["button-leave"]} onClick={()=>setCheckout(false)}>Leave</button>
+    <div className={styles["page"]} id= "page">
+      <p className={styles["page--title"]} id= "page--title">Changed your mind?</p>
+      <div className={styles["page--content-container"]} id= "page--content-container">
+        <p className={styles["page--content"]} id= "page--content">Are you sure you want to leave checkout, and go back to ? The items you've selected may not be available later.</p>
+        <div className={styles["page--buttons-containers"]} id= "page--buttons-containers">
+          <button className={styles["button-stay"]} onClick={() => setShowBackToTicketsMessage(false)} id= "button-stay">Stay</button>
+          <button className={styles["button-leave"]} onClick={() => setCheckout(false)} id= "button-leave">Leave</button>
         </div>
       </div>
     </div>
--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -30,55 +30,55 @@ export function Booking({event, ticketsType, setTicketsType, checkout, setChecko
 
 
   return (
-    <div className={styles["booking"]}>
-      <div className={styles["booking--content"]}>
+    <div className={styles["booking"]} id="booking">
+      <div className={styles["booking--content"]} id="booking--content">
 
-        <div className={styles["content--header"]}>
+        <div className={styles["content--header"]} id="content--header">
 
-          <p className={styles["header--event-title"]}>{event.name}</p>
-          <p className={styles["header--event-date"]}>{`${(new Date(event.startDate)).toLocaleDateString('en-US', { weekday: 'long' })}, ${event.startMonthInWords} ${event.startDay} · ${event.startHour} - ${event.endHour}`}</p>
+          <p className={styles["header--event-title"]} id="header--event-title">{event.name}</p>
+          <p className={styles["header--event-date"]} id="header--event-date">{`${(new Date(event.startDate)).toLocaleDateString('en-US', { weekday: 'long' })}, ${event.startMonthInWords} ${event.startDay} · ${event.startHour} - ${event.endHour}`}</p>
 
         </div>
 
-        <div className={styles["content--body"]}>
-          <div className={styles["body--tickets"]}>
-            <h3 className={styles["tickets--title"]}>Tickets</h3>
+        <div className={styles["content--body"]} id="content--body">
+          <div className={styles["body--tickets"]} id="body--tickets">
+            <h3 className={styles["tickets--title"]} id="tickets--title">Tickets</h3>
             {ticketsType.map(ticketType => (
               <TicketsContainer name={ticketType.name} type={ticketType.type} price={ticketType.price} capacity={ticketType.capacity}
                 reserved={ticketType.currentReservations} endDate={ticketType.sellingEndTime} ticketId={ticketType._id} ticketsType={ticketsType} setTicketsType={setTicketsType} />
             ))}
 
-            <div className={styles["tickets--footer"]}>
-              <span className={styles["footer--powered-by"]}>Powered by </span>
-              <span className={styles["footer--hebtus"]}>hebtus</span>
+            <div className={styles["tickets--footer"]} id="tickets--footer">
+              <span className={styles["footer--powered-by"]} id="footer--powered-by">Powered by </span>
+              <span className={styles["footer--hebtus"]} id="footer--hebtus">hebtus</span>
             </div>
           </div>
 
         </div>
 
-        <div className={styles["content--footer"]}>
-          <div className={styles["footer--error-container"]}>
+        <div className={styles["content--footer"]} id="content--footer">
+          <div className={styles["footer--error-container"]} id="footer--error-container">
             {!checkout &&
-              <div className={styles["error-empty"]}>
+              <div className={styles["error-empty"]} id="error-empty">
                 {errorEmpty ==="" ? "":<ErrorIcon className={styles["error-empty--icon"]} />}{errorEmpty}</div>
             }
           </div>
-          <div className={styles["chekout-btn-container"]}>
-            <button className={styles["footer--checkout-btn"]} onClick={() => { checkoutSubmit(ticketsType) }}>Checkout</button>
+          <div className={styles["chekout-btn-container"]} id="chekout-btn-container">
+            <button className={styles["footer--checkout-btn"]} onClick={() => { checkoutSubmit(ticketsType) }} id="footer--checkout-btn">Checkout</button>
           </div>
         </div>
 
       </div>
 
-      <div className={styles["booking--side"]}>
-        <div className={styles["side--event-image-container"]}>
-          <img src={event.img_url} alt="eventImage" className={styles["event-image"]} />
+      <div className={styles["booking--side"]} id="booking--side">
+        <div className={styles["side--event-image-container"]} id="side--event-image-container">
+          <img src={event.img_url} alt="eventImage" className={styles["event-image"]} id="event-image"/>
           <button className={styles["close-button"]} id='close-button' onClick={() => { setModal(false) }}>
             x
           </button>
         </div>
 
-        <div className={styles["side--order-summary"]}>
+        <div className={styles["side--order-summary"]} id ="side--order-summary">
           <OrderSummary ticketsType={ticketsType}/>
         </div>
 
